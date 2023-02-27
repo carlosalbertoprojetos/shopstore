2d00186cafc7527860b58145a3593f9e68791808
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2023-02-27 15:45:03 -0300

    'some_code_smell'

--- financial/admin.py
+++ financial/admin.py
@@ -1,29 +1,2 @@
 from django.contrib import admin
 
-# from .models import Bank, Account, Release, DestinationCategory, Destination
-
-
-
-# @admin.register(Bank)
-# class BankAdmin(admin.ModelAdmin):
-#     ...
-
-
-# @admin.register(Account)
-# class AccountAdmin(admin.ModelAdmin):
-#     ...
-
-
-# @admin.register(Release)
-# class ReleaseAdmin(admin.ModelAdmin):
-#     ...
-
-
-# @admin.register(DestinationCategory)
-# class DestinationCategoryAdmin(admin.ModelAdmin):
-#     ...
-
-
-# @admin.register(Destination)
-# class DestinationAdmin(admin.ModelAdmin):
-#     ...
